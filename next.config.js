cad90e97eaec03116d8c7dba8da4adf695990958
Lagani21 <lpatel@ncsu.edu>
2025-02-13 18:58:51 -0500

    added next.config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-// next.config.mjs
-const isProd = process.env.NODE_ENV === 'production';
-
-export default {
-  basePath: isProd ? '/laganipatel' : '', // Replace 'my-portfolio' with your GitHub repo name
-  assetPrefix: isProd ? '/laganipatel/' : '', // Same as above
+// next.config.js
+const nextConfig = {
+  experimental: {
+    appDir: true, // Enable the App Router
+  },
 };
+
+module.exports = nextConfig;
